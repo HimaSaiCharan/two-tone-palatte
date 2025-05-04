aee12882d211c88b574dd02465d19ef57274ec87
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-05-04 23:12:00 +0530

    view the palatte on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,117 @@
 import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return <>Hello world</>;
+class Color extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div
+        className={`color ${this.props.position}`}
+        style={{ background: this.props.color }}
+      >
+        {this.props.name}
+      </div>
+    );
+  }
+}
+
+class TwoTonePalatte extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div
+        className="palatte-container"
+        onClick={() => this.props.onClick(this.props.palatte)}
+      >
+        <div className="palatte">
+          <Color color={this.props.palatte.colors[0]} key={0} position="left" />
+          <Color
+            color={this.props.palatte.colors[1]}
+            key={1}
+            position="right"
+          />
+        </div>
+        <div className="color-codes">
+          <p className="color-code">{this.props.palatte.colors[0]}</p>
+          <p className="color-code">{this.props.palatte.colors[1]}</p>
+        </div>
+        <p className="palatte-name">{this.props.palatte.name}</p>
+      </div>
+    );
+  }
+}
+
+class PalattesContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="palattes-container">
+        {this.props.colorPalattes.map((palatte, i) => (
+          <TwoTonePalatte
+            palatte={palatte}
+            key={i}
+            onClick={this.props.onClick}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+class PalatteView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="palatte-view">
+        {this.props.colors.map((color, i) => (
+          <Color color={color} key={i} name={this.props.palatteName} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      palatteView: this.props.colorPalattes[0].colors,
+    };
+    this.updateView = this.updateView.bind(this);
+  }
+
+  updateView(palatte) {
+    this.setState({ palatteView: palatte.colors });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <PalattesContainer
+          colorPalattes={this.props.colorPalattes}
+          onClick={this.updateView}
+        />
+        <PalatteView
+          colors={this.state.palatteView}
+          name={this.state.palatteName}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,25 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <App
+    colorPalattes={[
+      { name: "Sunset", colors: ["#ff9b8a", "#fad1c5"] },
+      { name: "Ocean Breeze", colors: ["#00c9ff", "#93fe9f"] },
+      { name: "Lavender Feilds", colors: ["#b992d7", "#8ca6db"] },
+      { name: "Citrus Burst", colors: ["#f6d365", "#fda187"] },
+      { name: "Minty Fresh", colors: ["#a2fece", "#fbffd0"] },
+      { name: "Blush Bloom", colors: ["#fcb79e", "#feedd3"] },
+      { name: "Skyline", colors: ["#76b3ff", "#b69efe"] },
+      { name: "Peach Punch", colors: ["#feedd3", "#fcb79e"] },
+      { name: "Royal Violet", colors: ["#a38cd3", "#fcb79e"] },
+      { name: "Mossy Creek", colors: ["#a8edea", "#ffd7e3"] },
+      { name: "Coal Ash", colors: ["#bcc3c6", "#2d3f51"] },
+      { name: "Lemon ice", colors: ["#f7f9d5", "#e0c2fd"] },
+      { name: "Tangerine Twist", colors: ["#f83601", "#fbd525"] },
+      { name: "Clam Water", colors: ["#8af9ff", "#66a7fe"] },
+      { name: "Rosè Smoke", colors: ["#e8cac0", "#6271a4"] },
+      { name: "Stromy Sky", colors: ["#d6d2cd", "#314353"] },
+    ]}
+  />
+);
